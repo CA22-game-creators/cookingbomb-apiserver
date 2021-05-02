8fefc9940c9bd4c797dc3be2ccb719edad585d35
Karacky <mrnk3078@gmail.com>
2021-05-02 20:23:34 +0900

    doc: DBコメント修正

--- infrastructure/mysql/schemas/users.sql
+++ infrastructure/mysql/schemas/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-  `id` char(26) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザULID',
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザ名',
+  `id` char(26) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザーULID',
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザー名',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザ';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザー';
