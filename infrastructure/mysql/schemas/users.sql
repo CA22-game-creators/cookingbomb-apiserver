4b6b51a4421c8d7c352760faea578f292c7bf8fa
Karacky <mrnk3078@gmail.com>
2021-05-05 00:15:08 +0900

    change: hashed_auth_tokenに

--- infrastructure/mysql/schemas/users.sql
+++ infrastructure/mysql/schemas/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `users` (
   `id` char(26) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザーULID',
   `name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザー名',
-  `auth_token` char(60) COLLATE utf8mb4_bin NOT NULL COMMENT '認証トークンのハッシュ値',
+  `hashed_auth_token` char(60) COLLATE utf8mb4_bin NOT NULL COMMENT '認証トークンのハッシュ値',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザー';
