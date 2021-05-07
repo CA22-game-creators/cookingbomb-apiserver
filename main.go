13d9e2d0540c233bab4dcd2f1f497b9dae59e348
Karacky <mrnk3078@gmail.com>
2021-05-07 11:18:49 +0900

    feat: サーバー起動

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/CA22-game-creators/cookingbomb-proto/server/pb"
+
+	accountDI "github.com/CA22-game-creators/cookingbomb-apiserver/di/account"
 )
 
 func main() {
-	fmt.Print("Hello! World!")
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%s", os.Getenv("PORT")))
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	log.Printf("Listening :%s", os.Getenv("PORT"))
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAccountServicesServer(grpcServer, accountDI.DI())
+
+	reflection.Register(grpcServer)
+	if err = grpcServer.Serve(listen); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
