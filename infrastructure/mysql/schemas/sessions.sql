7510a0958ead16b87e7ff58ae88e3f21ac58c705
Karacky <mrnk3078@gmail.com>
2021-05-13 23:32:07 +0900

    feat:ユニークキー追加

--- infrastructure/mysql/schemas/sessions.sql
+++ infrastructure/mysql/schemas/sessions.sql
@@ -3,5 +3,6 @@ CREATE TABLE `sessions` (
   `session_token` char(36) COLLATE utf8mb4_bin NOT NULL COMMENT 'セッショントークン',
   `expired_at` datetime NOT NULL COMMENT 'セッションの有効期限',
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  UNIQUE KEY `uk_sessions_session_token` (`session_token`),
+  CONSTRAINT `fk_sessions_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='セッション';
