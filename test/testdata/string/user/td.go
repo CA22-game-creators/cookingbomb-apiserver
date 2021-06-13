d0ed225101df2045487c7b447f1b334289ed5a2f
唐木 稜生 <s14044@ca-20013110.cyberagent.co.jp>
2021-06-13 22:26:59 +0900

    faet: 名前のバリデーション変更

--- test/testdata/string/user/td.go
+++ test/testdata/string/user/td.go
@@ -2,15 +2,14 @@ package testdata
 
 import (
 	"strings"
-
-	"github.com/CA22-game-creators/cookingbomb-apiserver/domain/model/user"
 )
 
 var Name = struct {
-	Valid, Invalid, TooShort, TooLong string
+	Valid, Invalid, InvalidChar, TooShort, TooLong string
 }{
-	Valid:    "name",
-	Invalid:  string([]byte{0xff, 0xfe, 0xfd}),
-	TooShort: strings.Repeat("a", user.NameMinLen-1),
-	TooLong:  strings.Repeat("a", user.NameMaxLen+1),
+	Valid:       "name",
+	Invalid:     `\m\t@#$%`,
+	InvalidChar: string([]byte{0xff, 0xfe, 0xfd}),
+	TooShort:    "",
+	TooLong:     strings.Repeat("a", 100),
 }
